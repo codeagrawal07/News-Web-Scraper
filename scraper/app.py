e1a3e99f90eb6f92d9b29f66613e8c580f09851f
Abhishek Agrawal <148644857+codeagrawal07@users.noreply.github.com>
2026-01-28 13:20:53 +0530

    Add files via upload

---
+++ scraper/app.py
@@ -0,0 +1,140 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import article_parser
+import requests
+import random
+
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+]
+
+def scrape_page(url):
+    headers = {
+        'User-Agent': random.choice(user_agents)
+    }
+    session = requests.Session()
+    session.headers.update(headers)
+    response=session.get(url)
+    
+    return response.text
+
+
+st.set_page_config(page_title="News Scraper", layout="wide")
+
+st.title("📰 News Scraper Dashboard")
+
+url = st.text_input("Enter News Article URL")
+
+def id(url):
+    """
+    Detect Indian news source from article URL
+    Returns: 'indiatoday', 'hindustantimes', 'zeenews' or None
+    """
+    domain = urlparse(url).netloc.lower()
+
+    if "indiatoday.in" in domain:
+        return 1
+
+    if "hindustantimes.com" in domain:
+        return 2
+
+    if "zeenews.india.com" in domain or "zeenews.com" in domain:
+        return 3
+
+    return 0
+temp=0
+indToday=id(url)
+if indToday==1:
+    temp=1
+index=id(url) 
+  
+if st.button("Fetch News") and url:
+    with st.spinner("Fetching article..."):
+        htmlContent = scrape_page(url)
+        soup = BeautifulSoup(htmlContent, "html.parser")
+
+        title = article_parser.get_Title(soup)
+        date = article_parser.get_Date(soup)
+        author=article_parser.get_author(soup)
+        desc = article_parser.shor_description(soup)
+        content = article_parser.get_Context(index,soup)
+        images = article_parser.get_image(index,soup)
+        social_links = article_parser.get_social_media_Link(temp,soup)
+
+    with st.expander(title):
+        st.subheader(f"Title: {title}")
+        st.markdown(f"**Date:** {date}")
+        st.markdown(f"**Author:** {author}")
+        st.markdown(f"**Description:** {desc}")
+
+        st.subheader("Article Content")
+        st.write(content)
+
+        if images:
+            st.subheader("🖼 Images")
+            for img in images:
+                if img["imgURL"]:
+                    st.image(
+                        img["imgURL"],
+                        caption=img["imgTitle"] or img["imgAlt"],
+                        use_column_width=True
+                    )
+
+        if social_links:
+            st.subheader("📺 Embedded Media")
+            for media in social_links:
+                # st.write(media['link'])
+                if media["link"]:
+                   
+                    if media['source']=='instagram':
+                            st.write("instagram")
+                            components.html(
+                                f"""
+                                <blockquote class="instagram-media"
+                                    data-instgrm-permalink="{media['link']}"
+                                    data-instgrm-version="14">
+                                </blockquote>
+                                <script async src="//www.instagram.com/embed.js"></script>
+                                """,
+                                height=600
+                                )
+                    if media['source'] == 'reddit':
+                            st.write("reddit")
+                            components.html(
+                                f"""
+                                <blockquote class="reddit-embed-bq"
+                                    style="height:500px"
+                                    data-embed-height="500">
+                                <a href="{media['link']}"></a>
+                                </blockquote>
+                                <script async src="https://embed.reddit.com/widgets.js" charset="UTF-8"></script>
+                                """,
+                                height=550
+                            )
+                    if media['source'] == 'twitter':
+                            st.write("twitter")
+                            components.html(
+                                f"""
+                                <blockquote class="twitter-tweet">
+                                <a href="{media['link']}"></a>
+                                </blockquote>
+                                <script async src="https://platform.twitter.com/widgets.js"
+                                        charset="utf-8"></script>
+                                """,
+                                height=550
+                            )
+ 
+                    if media['source'] == 'other' :
+                            st.write("other") 
+                            components.iframe(
+                            src=media["link"],
+                            width=400,
+                            height=400,
+                            scrolling=True
+                        )
---
+++ scraper/article_parser.py
@@ -0,0 +1,306 @@
+import json
+from bs4 import BeautifulSoup,NavigableString
+import re
+def get_date_in_json(soup):
+    json_ld_scripts = soup.find_all("script", type="application/ld+json")
+
+    for script in json_ld_scripts:
+        if not script.string:
+            continue
+
+        data = json.loads(script.string)
+        if not isinstance(data, dict):
+            continue
+
+        # datePublished exists only in NewsArticle
+        if data.get("@type") == "NewsArticle":
+            return data.get("datePublished")[:10]
+
+    return None
+
+def get_Title(soup):
+    
+    og = soup.find('meta', {"property": "og:title"})
+    if og and og.get("content"):
+        return og["content"]
+
+    h1 = soup.find("h1")
+    return h1.text.strip() if h1 else "Not Found"
+      
+def get_author(soup):
+    # INDIA TODAY
+    
+    author_divs = soup.find_all("div", class_="authdetaisl")
+    if author_divs:
+        return author_divs[0].get_text(strip=True)
+    
+    #ZEE NEWS
+    
+    zee_author = soup.find("span", class_="aaticleauthor_name")
+    if zee_author:
+        text=zee_author.get_text()
+        return text
+    
+    #HINDUSTAN TIMES
+    
+    small = soup.find("small")
+    if small:
+        return small.get_text(strip=True)
+    
+    return "Not Found"
+
+ 
+def get_Date(soup):
+
+    meta_date = soup.find("meta", {"property": "article:published_time"})
+    if meta_date:
+        date = meta_date.get("content", "")[:10]
+    else:
+        date=get_date_in_json(soup)
+    return date 
+
+def shor_description(soup):
+    short_desc = soup.find('meta', {"name": "description"})
+    if short_desc:
+        return short_desc["content"].strip()
+    else:
+        return "Not Found"
+
+
+
+def get_Context(id,soup):
+    if id==0:
+        content = []
+        for p in soup.find_all('p'):
+            
+                text=p.get_text()
+                if text:
+                    content.append(text)
+
+        article_text = "\n".join(content)
+        return article_text
+
+    content = []
+    if id==1:
+        for p in soup.find_all("p"):
+            for child in p.contents:
+
+                # stop when 
+                if getattr(child, "name", None) == "div" and "end_story" in child.get("class", []):
+                    return "\n".join(content)
+
+                # plain text
+                if isinstance(child, NavigableString):
+                    text = child.strip()
+                    if text:
+                        content.append(text)
+                # anchor / strong text
+                elif child.name in ["a", "strong","span"]:
+                    text = child.get_text(strip=True)
+                    if text:
+                        content.append(text)
+        return "\n".join(content)
+    if id==2:
+        article=soup.find_all(["div","p"], class_=["content","blogTitle liveBlogHdg"])
+        if article:
+            for p in article:
+                
+                # Case 1: Read More block
+                if p.find("strong") and "Read More" in p.get_text():
+                    None                 
+                # Case 2: Normal paragrap
+                if p.find('i'):
+                    None
+                if p.find('h2') and p.find('a'):
+                    text=(p.find('p')).get_text()
+                    if text:
+                        content.append(text)
+                else:
+                    text = p.get_text(strip=True)
+                    if text:
+                        content.append(text)
+                        
+            return  "\n".join(content)
+        else :
+            article=soup.find_all(["div","p"],id="fullIntroContent")
+            if article:
+                for p in article:
+                    
+                    # Case 1: Read More block
+                    if p.find("strong") and "Read More" in p.get_text():
+                        None                 
+                    # Case 2: Normal paragrap
+                    if p.find('i'):
+                        None
+                    if p.find('h2') and p.find('a'):
+                        text=(p.find('p')).get_text()
+                        if text:
+                            content.append(text)
+                    else:
+                        text = p.get_text(strip=True)
+                        if text:
+                            content.append(text)
+                            
+                return  "\n".join(content)
+                    
+    if id==3:
+        article = soup.find(id="fullArticle")
+        s=str(article)
+        soup1=BeautifulSoup(s,'html.parser')
+        container = soup1.find('div', id='fullArticle')
+        cleaned_lines = []
+        if container:
+        
+            for junk in container.find_all(['script', 'iframe', 'div', 'ins'], 
+                                        class_=['ads-box-300x250', 'recommended_widget', 'googlePopUp', 'mb-3', 'ads-placeholder-internal']):
+                junk.decompose()
+
+            # 3. Get all text with a double newline separator to keep it readable
+            # strip=True removes leading/trailing whitespace from each chunk
+            lines = container.get_text(separator="\n\n", strip=True).split('\n\n')
+
+            # 4. Filter out the specific lines you mentioned
+            for line in lines:
+                # Skip the Zee News CTA
+                if "Add Zee News as a Preferred Source" in line:
+                    continue
+                # Skip "Also Read" links
+                if line.startswith("Also Read-"):
+                    continue
+                # Skip Agency credits
+                if any(credit in line for credit in ["(with IANS inputs)", "(With ANI inputs)"]):
+                    continue
+                
+                cleaned_lines.append(line)
+        else:
+            print("Article container not found.")
+        return "\n".join(cleaned_lines) 
+    
+def get_image(id,soup):
+    
+    img_list=[]
+    try:
+        img_main_url = (soup.find("meta", {"property": "og:image"}))['content']
+        
+        if img_main_url:
+            img_url= img_main_url.split("/")[-1].split("?")[0]
+            img_tag = soup.find("img", src=lambda x: x and img_url in x)
+            if img_tag:
+                img_list.append({
+                    "imgURL":img_tag.get('src') or"",
+                    "imgTitle":img_tag.get('title') or"",
+                    "imgAlt":img_tag.get('alt') or"",
+                    "imgHeight":img_tag.get('height') or"",
+                    "imgWidth":img_tag.get('width') or"",})
+    except:
+          None            
+    if id==1:    
+        try:   
+            img_all=soup.find_all('div',class_="itgimage")
+            
+            if img_all:
+                for item in img_all:
+                    img_list.append({
+                        "imgURL":item.img.get('src') or"",
+                        "imgTitle":item.img.get('title') or"",
+                        "imgAlt":item.img.get('alt') or"",
+                        "imgHeight":item.img.get('height') or"",
+                        "imgWidth":item.img.get('width') or"",})
+            return img_list
+        except:
+            None
+    if id==2:
+        try:
+            img_all=soup.find_all(class_='artImage')
+            if img_all:
+                for item in img_all:
+                    img_list.append({
+                        "imgURL":item.img.get('data-src') or item.img.get('data') or "",
+                        "imgTitle":item.img.get('title') or"",
+                        "imgAlt":item.img.get('alt') or"" })
+            return img_list
+        except:
+            None
+    if id==3:
+        try:
+            img_all=soup.find_all("div",class_="photoimg_container")
+            if img_all:
+                for item in img_all:
+                    img_list.append({
+                        "imgURL":item.img.get('data-src') or item.img.get('data') or "" ,
+                        "imgTitle":item.img.get('title') or"",
+                        "imgAlt":item.img.get('alt') or"" })
+            return img_list
+        except:
+            None
+            
+                   
+def get_social_media_Link(id,soup):
+        list_of_links=[]
+        if id==1:
+            listOfArticles = soup.find_all('article')
+            if listOfArticles:
+                for article in listOfArticles:
+                    fram=article.find(['iframe'])
+                    if fram:
+                        list_of_links.append({
+                            "link":fram.get('src') or"",
+                            "source":"other"})
+            return list_of_links            
+        if id==0:               
+            listofReddit=soup.find_all("blockquote", class_="reddit-embed-bq")            
+            if listofReddit:
+                for post in listofReddit:
+                    link = post.a.get("href")
+                    if link:
+                        list_of_links.append({
+                            "link":link or"",
+                            "source":"reddit"})
+                        
+            listofinstagram=soup.find_all('blockquote',class_="instagram-media")
+            if listofinstagram:
+                for post in listofinstagram:
+                    link = post.get("data-instgrm-permalink")
+                    if link:
+                        list_of_links.append({
+                            "link":link or"",
+                            "source":"instagram"})
+            listofTweet=soup.find_all('div', class_='ht-twitter-embed')            
+            if listofTweet:
+                for post in listofTweet:
+                    link = post.get("data-twitter-src")
+                    if link:
+                        list_of_links.append({
+                            "link":link or"",
+                            "source":"twitter"})
+                        
+            listofTweetZee = soup.find_all('blockquote', class_="twitter-tweet")
+            if listofTweetZee:
+                for post in listofTweetZee:
+                    anchors = post.find_all('a')
+                    for a in anchors:
+                        href = a.get('href')
+                        if href and href.startswith("https://twitter.com/"):
+                            list_of_links.append({
+                                "link": href,
+                                "source": "twitter"
+                            })
+             
+      
+
+            fb_links = re.findall(
+                r'https://www\.facebook\.com/plugins[^"\']+',
+                str(soup)
+            )
+
+            fb_links = [
+                {"link": link, "source": "other"}
+                for link in set(fb_links)
+            ]
+            list_of_links.extend(fb_links)  
+                     
+            return list_of_links 
+                
+                
+                
+            
